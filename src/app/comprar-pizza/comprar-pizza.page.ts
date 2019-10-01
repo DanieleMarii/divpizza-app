2514e60e8ae85283559044ab6e299d8bf2f9f10a
MARIANE DANIELE SANTANA SILVA <570586148@alunos.etec.itaqua>
2019-09-30 22:24:16 -0300

    implementação do botão comprar

--- src/app/comprar-pizza/comprar-pizza.page.ts
+++ src/app/comprar-pizza/comprar-pizza.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-comprar-pizza',
@@ -9,14 +10,39 @@ import { ActivatedRoute } from '@angular/router';
 export class ComprarPizzaPage implements OnInit {
 
   idPizza;
-  constructor(private activatedRoute:ActivatedRoute) { }
+  nomePizza;
+  descricaoPizza;
+  precoPizza;
+  divisor;
+  precoPizzaFixo;
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
   ionViewDidEnter() {
     this.idPizza = this.activatedRoute.snapshot.params.id
-  }
+    let pizzaString = localStorage.getItem(this.idPizza)
+    let pizzaObjeto = JSON.parse(pizzaString)
+    this.nomePizza = pizzaObjeto.nomePizza;
+    this.descricaoPizza = pizzaObjeto.descricaoPizza;
+    this.precoPizza = pizzaObjeto.precoPizza;
+    this.precoPizzaFixo = this.precoPizza
 
+  }
+  incrementar() {
+    this.divisor++
+  }
+  decrementar() {
+    if (this.divisor > 1) {
+      this.divisor++
+    }
+  }
+  atualizaPreco() {
+    this.precoPizza = (parseFloat(this.precoPizzaFixo)
+      / this.divisor).toFixed(2)
+  }
+  atualizarComRange() {
+    this.atualizaPreco()
+  }
 }
-.
